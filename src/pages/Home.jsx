edeb94fe79092450d3a6eaf304a531dda0eff2e3
Emon Morol <ekramulhasane69@gmail.com>
2025-12-03 21:03:14 +0600

    changed gitignore

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
-  export default function Home() {
+export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center space-y-4">
       <h1 className="text-9xl font-bold flex items-center justify-center w-screen h-screen">ADVYON</h1>
     </div>
   );
-  }
+}
